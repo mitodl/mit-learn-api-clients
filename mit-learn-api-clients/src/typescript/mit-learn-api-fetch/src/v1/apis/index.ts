20b47d8af383eba7f3a67028600860039063a384
Nathan Levesque <rhysyngsun@gmail.com>
2025-10-21 17:04:23 -0400

    Add typescript-fetch API client for nextjs

---
+++ mit-learn-api-clients/src/typescript/mit-learn-api-fetch/src/v1/apis/index.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ArticlesApi';
+export * from './ContentFileSearchApi';
+export * from './ContentfilesApi';
+export * from './CourseFeaturesApi';
+export * from './CoursesApi';
+export * from './DepartmentsApi';
+export * from './FeaturedApi';
+export * from './LearningResourceDisplayInfoApi';
+export * from './LearningResourcesApi';
+export * from './LearningResourcesSearchApi';
+export * from './LearningResourcesUserSubscriptionApi';
+export * from './LearningpathsApi';
+export * from './OfferorsApi';
+export * from './PlatformsApi';
+export * from './PodcastEpisodesApi';
+export * from './PodcastsApi';
+export * from './ProgramLettersApi';
+export * from './ProgramsApi';
+export * from './SchoolsApi';
+export * from './TopicsApi';
+export * from './UserlistsApi';
+export * from './VideoPlaylistsApi';
+export * from './VideosApi';
+export * from './WebhooksApi';
