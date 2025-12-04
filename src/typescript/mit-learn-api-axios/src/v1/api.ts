238394016e011ba4466922c2d973b33d92d588c0
MIT Open Learning Engineering <odl-devops@mit.edu>
2025-12-04 20:48:21 +0000

    Generated clients for mit-learn from rev: 46f4961b515259de57ec13183a484b8900103075

--- src/typescript/mit-learn-api-axios/src/v1/api.ts
+++ src/typescript/mit-learn-api-axios/src/v1/api.ts
@@ -3665,6 +3665,19 @@ export type LevelEnum = typeof LevelEnum[keyof typeof LevelEnum];
 
 
 /**
+ * 
+ * @export
+ * @interface MediaUpload201Response
+ */
+export interface MediaUpload201Response {
+    /**
+     * 
+     * @type {string}
+     * @memberof MediaUpload201Response
+     */
+    'url'?: string;
+}
+/**
  * Serializer containing only parent and child ids for a learning path relationship
  * @export
  * @interface MicroLearningPathRelationship
@@ -4668,16 +4681,16 @@ export interface PatchedLearningResourceRelationshipRequest {
 export interface PatchedRichTextArticleRequest {
     /**
      * 
-     * @type {any}
-     * @memberof PatchedRichTextArticleRequest
-     */
-    'content'?: any;
-    /**
-     * 
      * @type {string}
      * @memberof PatchedRichTextArticleRequest
      */
     'title'?: string;
+    /**
+     * 
+     * @type {any}
+     * @memberof PatchedRichTextArticleRequest
+     */
+    'content'?: any;
 }
 /**
  * Serializer for UserListRelationship model
@@ -7006,12 +7019,6 @@ export type ResourceTypeEnum = typeof ResourceTypeEnum[keyof typeof ResourceType
 export interface RichTextArticle {
     /**
      * 
-     * @type {any}
-     * @memberof RichTextArticle
-     */
-    'content'?: any;
-    /**
-     * 
      * @type {number}
      * @memberof RichTextArticle
      */
@@ -7022,6 +7029,30 @@ export interface RichTextArticle {
      * @memberof RichTextArticle
      */
     'title': string;
+    /**
+     * 
+     * @type {any}
+     * @memberof RichTextArticle
+     */
+    'content'?: any;
+    /**
+     * 
+     * @type {User}
+     * @memberof RichTextArticle
+     */
+    'user': User;
+    /**
+     * 
+     * @type {string}
+     * @memberof RichTextArticle
+     */
+    'created_on': string;
+    /**
+     * 
+     * @type {string}
+     * @memberof RichTextArticle
+     */
+    'updated_on': string;
 }
 /**
  * Serializer for LearningResourceInstructor model
@@ -7031,16 +7062,16 @@ export interface RichTextArticle {
 export interface RichTextArticleRequest {
     /**
      * 
-     * @type {any}
-     * @memberof RichTextArticleRequest
-     */
-    'content'?: any;
-    /**
-     * 
      * @type {string}
      * @memberof RichTextArticleRequest
      */
     'title': string;
+    /**
+     * 
+     * @type {any}
+     * @memberof RichTextArticleRequest
+     */
+    'content'?: any;
 }
 /**
  * * `phrase` - phrase * `best_fields` - best_fields * `most_fields` - most_fields * `hybrid` - hybrid  * `phrase` - phrase * `best_fields` - best_fields * `most_fields` - most_fields * `hybrid` - hybrid
@@ -7223,6 +7254,25 @@ export type SourceTypeEnum = typeof SourceTypeEnum[keyof typeof SourceTypeEnum];
 
 
 /**
+ * 
+ * @export
+ * @interface User
+ */
+export interface User {
+    /**
+     * 
+     * @type {string}
+     * @memberof User
+     */
+    'first_name'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof User
+     */
+    'last_name'?: string;
+}
+/**
  * Simplified serializer for UserList model.
  * @export
  * @interface UserList
@@ -7374,6 +7424,25 @@ export interface UserListRequest {
 }
 
 
+/**
+ * 
+ * @export
+ * @interface UserRequest
+ */
+export interface UserRequest {
+    /**
+     * 
+     * @type {string}
+     * @memberof UserRequest
+     */
+    'first_name'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof UserRequest
+     */
+    'last_name'?: string;
+}
 /**
  * Serializer for the Video model
  * @export
@@ -18881,6 +18950,131 @@ export const LearningpathsListSortbyEnum = {
     Views2: 'views'
 } as const;
 export type LearningpathsListSortbyEnum = typeof LearningpathsListSortbyEnum[keyof typeof LearningpathsListSortbyEnum];
+
+
+/**
+ * MediaApi - axios parameter creator
+ * @export
+ */
+export const MediaApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * Upload an image (multipart/form-data) and return the storage URL.
+         * @param {File} image_file 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        mediaUpload: async (image_file: File, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'image_file' is not null or undefined
+            assertParamExists('mediaUpload', 'image_file', image_file)
+            const localVarPath = `/api/v1/upload-media/`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+            const localVarFormParams = new ((configuration && configuration.formDataCtor) || FormData)();
+
+
+            if (image_file !== undefined) { 
+                localVarFormParams.append('image_file', image_file as any);
+            }
+    
+    
+            localVarHeaderParameter['Content-Type'] = 'multipart/form-data';
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            localVarRequestOptions.data = localVarFormParams;
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * MediaApi - functional programming interface
+ * @export
+ */
+export const MediaApiFp = function(configuration?: Configuration) {
+    const localVarAxiosParamCreator = MediaApiAxiosParamCreator(configuration)
+    return {
+        /**
+         * Upload an image (multipart/form-data) and return the storage URL.
+         * @param {File} image_file 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async mediaUpload(image_file: File, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<MediaUpload201Response>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.mediaUpload(image_file, options);
+            const index = configuration?.serverIndex ?? 0;
+            const operationBasePath = operationServerMap['MediaApi.mediaUpload']?.[index]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, operationBasePath || basePath);
+        },
+    }
+};
+
+/**
+ * MediaApi - factory interface
+ * @export
+ */
+export const MediaApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    const localVarFp = MediaApiFp(configuration)
+    return {
+        /**
+         * Upload an image (multipart/form-data) and return the storage URL.
+         * @param {MediaApiMediaUploadRequest} requestParameters Request parameters.
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        mediaUpload(requestParameters: MediaApiMediaUploadRequest, options?: RawAxiosRequestConfig): AxiosPromise<MediaUpload201Response> {
+            return localVarFp.mediaUpload(requestParameters.image_file, options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * Request parameters for mediaUpload operation in MediaApi.
+ * @export
+ * @interface MediaApiMediaUploadRequest
+ */
+export interface MediaApiMediaUploadRequest {
+    /**
+     * 
+     * @type {File}
+     * @memberof MediaApiMediaUpload
+     */
+    readonly image_file: File
+}
+
+/**
+ * MediaApi - object-oriented interface
+ * @export
+ * @class MediaApi
+ * @extends {BaseAPI}
+ */
+export class MediaApi extends BaseAPI {
+    /**
+     * Upload an image (multipart/form-data) and return the storage URL.
+     * @param {MediaApiMediaUploadRequest} requestParameters Request parameters.
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof MediaApi
+     */
+    public mediaUpload(requestParameters: MediaApiMediaUploadRequest, options?: RawAxiosRequestConfig) {
+        return MediaApiFp(this.configuration).mediaUpload(requestParameters.image_file, options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
 
 
 /**
